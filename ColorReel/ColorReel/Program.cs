f9f147b269a93e5001ca098f1122ce241f6febee
16bitsorcerer <faramirez.chicago@gmail.com>
2024-04-03 17:24:12 -0500

    First commit

--- ColorReel/ColorReel/Program.cs
+++ ColorReel/ColorReel/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace ColorReel;
+﻿using System.Diagnostics;
+using System.Runtime.ConstrainedExecution;
+using System;
+
+
+namespace ColorReel;
 
 class Program
 {
     static void Main(string[] args)
     {
+        // Francisco Ramirez
+        // 03/25/24
+        // Color Reel: This is an app for developing poster art. Set the options below and watch the HTML we can generate!
+
+
+
         // Step 1: Define the core concept of the program - generating movie posters with colored barcodes.
 
         // Step 2: Design the user interface (UI) for the application using HTML, CSS, and JavaScript.
@@ -31,7 +42,52 @@ class Program
         // across different devices and screen sizes.
         // Gather feedback from users and iterate on the design and functionality based on user testing results.
 
+        string[] menu = { "Size", "Bar", "Layout", "Color to Replace", "Movie", "Image Path" };
+        int menuIndex = 0;
 
+        while (true)
+        {
+            Console.Clear();
+
+            DisplayMenuOptions(menu, menuIndex);
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.W:
+                    if (menuIndex > 0)
+                        menuIndex--;
+                    break;
+                case ConsoleKey.DownArrow:
+                case ConsoleKey.S:
+
+                    if (menuIndex < menu.Length - 1)
+                        menuIndex++;
+                    break;
+                case ConsoleKey.Enter:
+                    Console.Clear();
+                    Console.WriteLine("Selected option: " + menu[menuIndex]);
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey(true);
+                    break;
+            }
+        }
     }
-}
 
+
+    static void DisplayMenuOptions(string[] options, int index)
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            if (i == index)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine(options[i]);
+            Console.ResetColor();
+        }
+    }
+
+}
